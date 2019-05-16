d29cdccb0abee7ce8063a1bb04a76036848a7c36
Fernando Barahona <fernandobarahonad@gmail.com>
2019-05-16 16:40:40 -0500

    creadas las bases para el ruteo
    Solucione el problema del codigo base de los <select> en materialize.
    
    En App.js:  cambie GET por POST y setee para enviar como parametro los filtros de busqueda send(filtros). E hice una pagina en caso de que no haya resultados.
    
    En Index.js: recibi los parametros de POST y cree un if para devolver la info segun los parametros.
    
    En pocas palabras setee las bases para hacer routing solo con XMLHttpRequest y http.createServer.
    
    Ordene y comente el codigo.
    
    Tambien cree un archivo para tener almacenadas las cabeceras de CORS por si las necesite.

--- public/app.js
+++ public/app.js
@@ -1,6 +1,12 @@
-//Inicializador del elemento Slider
+//inicializador de las etiquetas SELECT en MATERIALIZA
+$(document).ready(function(){
+  $('select').material_select();
+});
+
+//declaracion de las VARIABLES GLOBALES definidas en state
 const state = {};
 
+//Inicializador del elemento Slider 
 $("#rangoPrecio").ionRangeSlider({
   type: "double",
   grid: false,
@@ -9,85 +15,103 @@ $("#rangoPrecio").ionRangeSlider({
   from: 1000,
   to: 20000,
   prefix: "$"
-})
+});
 
-function setSearch() {
-  let busqueda = $('#checkPersonalizada')
-  busqueda.on('change', (e) => {
-    if (this.customSearch == false) {
-      this.customSearch = true
-    } else {
-      this.customSearch = false
-    }
-    $('#personalizada').toggleClass('invisible')
-  })
-}
+//Funcionalidad de BOTON DE BUSQUEDA para presentar campos de busqueda 
+$('#checkPersonalizada').on('change', (e) => {
+  if (this.customSearch == false) {
+    this.customSearch = true
+  } else {
+    this.customSearch = false
+  }
+  $('#personalizada').toggleClass('invisible')
+});
 
-const hacerPeticiones = async () =>{
-  await requerirPeticiones();
-  console.log(state.respuestaJSON);
+$('#buscar').click((e) => { 
+  hacerPeticiones('chao');
+});
 
+//Funcion para requerir peticiones, esperar y devolverlas en la pantalla
+const hacerPeticiones = async (filtros) =>{
+  await requerirPeticiones(filtros);
   presentarPeticiones();
 };
-
-const requerirPeticiones = async () => {
+//funcion para requerir peticiones
+const requerirPeticiones = async (filtros) => {
   return new Promise(function (resolve, reject) {
     const request = new XMLHttpRequest;
-    request.open('GET', "http://127.0.0.1:1337/",true);
+    request.open('POST', "http://127.0.0.1:1337/",true);
     request.onload = function () {
       if (this.status == 200){
         resolve();
-        state.respuestaJSON = JSON.parse(request.response);
+        state.respuesta = request.response;
       }else if (this.status == 404){
         reject();
         console.log('readystate onLoad', request);
       }
     }
-    request.send();
+    request.send(filtros);
   });
 };
 
-
+//funcion para presentar los resultados de peticiones en HTML
 const presentarPeticiones = () => {
   listaResultados = document.querySelector('.lista');
+  listaResultados.innerHTML = '';
   
-  state.respuestaJSON.forEach(element => {
-    elementoHTML = `
-      <div class="card horizontal">
-      <div class="card-image">
-        <img src="./img/home.jpg">
-      </div>
-      <div class="card-stacked">
-        <div class="card-content">
-          <div>
-            <b>Direccion: </b><p>${element.Direccion}</p>
-          </div>
-          <div>
-            <b>Ciudad: </b><p>${element.Ciudad}</p>
-          </div>
-          <div>
-            <b>Telefono: </b><p>${element.Telefono}</p>
-          </div>
-          <div>
-            <b>Código postal: </b><p>${element.Codigo_Postal}</p>
-          </div>
-          <div>
-            <b>Precio: </b><p>${element.Precio}</p>
-          </div>
-          <div>
-            <b>Tipo: </b><p>${element.Tipo}</p>
-          </div>          
+  if (state.respuesta) {
+    JSON.parse(state.respuesta).forEach(element => {
+      elementoHTML = `
+        <div class="card horizontal">
+        <div class="card-image">
+          <img src="./img/home.jpg">
         </div>
-        <div class="card-action right-align">
-          <a href="#">Ver más</a>
+        <div class="card-stacked">
+          <div class="card-content">
+            <div>
+              <b>Direccion: </b><p>${element.Direccion}</p>
+            </div>
+            <div>
+              <b>Ciudad: </b><p>${element.Ciudad}</p>
+            </div>
+            <div>
+              <b>Telefono: </b><p>${element.Telefono}</p>
+            </div>
+            <div>
+              <b>Código postal: </b><p>${element.Codigo_Postal}</p>
+            </div>
+            <div>
+              <b>Precio: </b><p>${element.Precio}</p>
+            </div>
+            <div>
+              <b>Tipo: </b><p>${element.Tipo}</p>
+            </div>          
+          </div>
+          <div class="card-action right-align">
+            <a href="#">Ver más</a>
+          </div>
         </div>
+        </div>
+        `;
+        listaResultados.insertAdjacentHTML('beforeend', elementoHTML);
+    });
+  } else{
+    resultadoVacioHtml = `
+    <div class="card horizontal">
+    <div class="card-image">
+      <img src="./img/home.jpg">
+    </div>
+    <div class="card-stacked">
+      <div class="card-content">
+        <div>
+          <p>NO SE ENCONTRARON LOS ELEMENTOS DESEADOS</p>
+        </div>         
       </div>
-      </div>
-      `;
-      listaResultados.insertAdjacentHTML('beforeend', elementoHTML);
-  });
+    </div>
+    </div>
+    `;
+
+    listaResultados.insertAdjacentHTML('beforeend', resultadoVacioHtml);
+  }
 };
-
-setSearch();
-hacerPeticiones();
-
+hacerPeticiones('hola');
--- server/index.js
+++ server/index.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
 const http = require('http');
-const url = require('url');
 const PORT = 1337;
 
+//leo los datos del archivo data.json (lo leao sync porque solo lo necesito una vez al cargar el servidor)
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
-const dataJSON = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-    //permitit el acceso al servidor configurando CORS
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+//creo el servidor desde el modulo nativo de node http
+const server = http.createServer(async (req, res) => {
+    
+    //EXPLICACION DE RECIBIR DATOS: cuando usamos createServer le pasamos un callback que recibe req y res. Req es un objeto que viene de la peticion de cliente y res es un objeto de este tipo especifico al cual le damos ciertas caracteristicas para enviarlo con end(); ---- Ahora, el objeto REQ es de tipo http.ClientRequest que implementa stream.Writable y todos los streams son instancias de EventEmitter. Event emitter tiene una funcion para escuchar un evento llamada .on y .once que es igual pero solo eschucha una vez y se cierra. En este caso escuchan que llego el data y el callback recibe el pedazo de data (por eso se llaman streams) y lo anade a los otros pedazos de data. 
+
+    //recibir datos del cliente cuando es POST (lo que se manda en request.send)
+    var parametrosCliente = '';
+    await (()=> {
+        req.once('data', function (chunk) {
+            parametrosCliente += chunk;
+            console.log(parametrosCliente);
+        });
+    })();
 
     //escribir cabezera
     res.writeHead(200, {
         'Content-Type': 'text/json',
+        //sire para evitar los problemas de CORS
 		'Access-Control-Allow-Origin': '*',
 		'X-Powered-By':'nodejs'
     });
-    res.write(data);
+
+    if (parametrosCliente === "chao"){
+        res.write(data);
+    } else {
+        res.write('');
+    }
+
+    //enviar la respuesta
     res.end(); 
     
     console.log('respuesta entregada');
