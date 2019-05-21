1815826bd810b990ab22d28da95c6b44f1791f6f
Fernando Barahona <fernandobarahonad@gmail.com>
2019-05-20 21:06:40 -0500

    final
    Complete las funcionalidades de los listeners en app.js y la forma de mandar los datos mediante el estado de la pagina.
    
    y complete el routing del servidor para enviar los datos filtrados, con variables que solo se crean al procesar la peticion.

--- public/app.js
+++ public/app.js
@@ -1,34 +1,18 @@
-//inicializador de las etiquetas SELECT en MATERIALIZA
-$(document).ready(function(){
-  $('select').material_select();
-});
+//--------DECLARACION de las VARIABLES GLOBALES definidas en state
+//----------------------------------------------------------------
+const state = {
+  customSearch: false,
+  respuesta: "",
+  filtros: {
+    "Ciudad" : "",
+    "Tipo" : "",
+    "precioMin" : "",
+    "precioMax" : ""
+}
+};
 
-//declaracion de las VARIABLES GLOBALES definidas en state
-const state = {};
-
-//Inicializador del elemento Slider 
-$("#rangoPrecio").ionRangeSlider({
-  type: "double",
-  grid: false,
-  min: 0,
-  max: 100000,
-  from: 1000,
-  to: 20000,
-  prefix: "$"
-});
-
-//Funcionalidad de BOTON DE BUSQUEDA para presentar campos de busqueda 
-$('#checkPersonalizada').on('change', (e) => {
-
-  if (state.customSearch == false || state.customSearch == undefined) {
-    state.customSearch = true;
-    $('#buscar').text('BUSCAR')
-  } else {
-    state.customSearch = false;
-    $('#buscar').text('VER TODOS')
-  }
-  $('#personalizada').toggleClass('invisible')
-});
+//--------DECLARACION de FUNCIONES a utilizarse
+//----------------------------------------------------------------
 
 //Funcion para requerir peticiones, esperar y devolverlas en la pantalla
 const hacerPeticiones = async (filtros) =>{
@@ -61,34 +45,34 @@ const presentarPeticiones = () => {
   if (state.respuesta) {
     JSON.parse(state.respuesta).forEach(element => {
       elementoHTML = `
-        <div class="card horizontal">
-        <div class="card-image">
-          <img src="./img/home.jpg">
-        </div>
-        <div class="card-stacked">
-          <div class="card-content">
+      <div class="card horizontal">
+      <div class="card-image">
+      <img src="./img/home.jpg">
+      </div>
+      <div class="card-stacked">
+      <div class="card-content">
+      <div>
+      <b>Direccion: </b><p>${element.Direccion}</p>
+      </div>
+      <div>
+      <b>Ciudad: </b><p>${element.Ciudad}</p>
+      </div>
+      <div>
+      <b>Telefono: </b><p>${element.Telefono}</p>
+      </div>
+      <div>
+      <b>Código postal: </b><p>${element.Codigo_Postal}</p>
+      </div>
             <div>
-              <b>Direccion: </b><p>${element.Direccion}</p>
-            </div>
-            <div>
-              <b>Ciudad: </b><p>${element.Ciudad}</p>
-            </div>
-            <div>
-              <b>Telefono: </b><p>${element.Telefono}</p>
-            </div>
-            <div>
-              <b>Código postal: </b><p>${element.Codigo_Postal}</p>
-            </div>
-            <div>
-              <b>Precio: </b><p>${element.Precio}</p>
+            <b>Precio: </b><p>${element.Precio}</p>
             </div>
             <div>
               <b>Tipo: </b><p>${element.Tipo}</p>
-            </div>          
-          </div>
-          <div class="card-action right-align">
+              </div>          
+              </div>
+              <div class="card-action right-align">
             <a href="#">Ver más</a>
-          </div>
+            </div>
         </div>
         </div>
         `;
@@ -98,43 +82,89 @@ const presentarPeticiones = () => {
     resultadoVacioHtml = `
     <div class="card horizontal">
     <div class="card-image">
-      <img src="./img/home.jpg">
+    <img src="./img/home.jpg">
     </div>
     <div class="card-stacked">
-      <div class="card-content">
-        <div>
-          <p>NO SE ENCONTRARON LOS ELEMENTOS DESEADOS</p>
-        </div>         
-      </div>
+    <div class="card-content">
+    <div>
+    <p>NO SE ENCONTRARON LOS ELEMENTOS DESEADOS</p>
+    </div>         
+    </div>
     </div>
     </div>
     `;
-
+    
     listaResultados.insertAdjacentHTML('beforeend', resultadoVacioHtml);
   }
 };
 
-//inicializar los <option> de los <select> de ciudad y tipo
-const iniciarOptions = async () =>{
-  await requerirPeticiones('ciudades');
-  const ciudadesString = state.respuesta;
-  const ciudades = ciudadesString.split(",");
-  ciudades.forEach((el) => {
-    document.querySelector('#ciudad').insertAdjacentHTML('beforeend',`<option value="${el}">${el}</option>`)
-  });
-  await requerirPeticiones('tipos');
-  const tiposString = state.respuesta;
-  const tipos = tiposString.split(",");
-  tipos.forEach((el) => {
-    document.querySelector('#tipo').insertAdjacentHTML('beforeend',`<option value="${el}">${el}</option>`)
-  });
-  $('select').material_select();
+//funciones SETEAR FILTROS cuando se da click en BUSQUEDA PERSONALIZADA
+const encerarFiltros = () => {
+  state.filtros.Ciudad = "";
+  state.filtros.Tipo = "";
+  state.filtros.precioMin = "$1000";
+  state.filtros.precioMax = "$100000";
 };
-iniciarOptions();
+const setearFiltros = () =>{
+ 
+    state.filtros.Ciudad = document.querySelector("#ciudad option:checked").value;
+    state.filtros.Tipo = document.querySelector('#tipo option:checked').value;
+    state.filtros.precioMin = document.querySelector('.irs-from').innerText;
+    state.filtros.precioMax = document.querySelector('.irs-to').innerText;
+    if (state.filtros.precioMin == "0" || state.filtros.precioMin == "") state.filtros.precioMin = "$1000";
+    if (state.filtros.precioMax == "0" || state.filtros.precioMax == "") state.filtros.precioMax = "$20000";
+};
+//--------SETEO INICIAL DEL PROGRAMA
+//----------------------------------------------------------------
 
-//escuchar el click en buscar y 
-$('#buscar').click((e) => { 
-  hacerPeticiones('chao');
+//inicializar los <option> de los <select> de ciudad y tipo
+(async () =>{
+  await ['Ciudad','Tipo'].forEach(async (element) => {
+    await requerirPeticiones(element); 
+    const opciones = state.respuesta.split(",");
+    opciones.forEach((el) => {
+      document.querySelector(`#${element.toLowerCase()}`).insertAdjacentHTML('beforeend',`<option value="${el}">${el}</option>`);
+    });
+    //inicializador de las etiquetas SELECT en MATERIALIZA
+    $('select').material_select();
+  });
+})();
+
+//Inicializador del elemento Slider 
+$("#rangoPrecio").ionRangeSlider({
+  type: "double",
+  grid: false,
+  min: 0,
+  max: 100000,
+  from: 1000,
+  to: 20000,
+  prefix: "$"
 });
 
-hacerPeticiones('hola');
+//--------CORRER LISTENERS
+//----------------------------------------------------------------
+//NOTA: LOS LISTENERS DE CIUDAD Y TIPO SE ANADEN CUANDO SE AGREGAN LAS <OPTIONS>
+
+//Funcionalidad de boton de BUSQUEDA PERSONALIZADA para presentar campos de busqueda 
+$('#checkPersonalizada').on('change', (e) => {
+
+  if (state.customSearch == false) {
+    state.customSearch = true;
+    $('#buscar').text('BUSCAR');
+  } else {
+    state.customSearch = false;
+    $('#buscar').text('VER TODOS')
+  }
+  $('#personalizada').toggleClass('invisible')
+});
+
+//escuchar el click en BOTON BUSCAR, para solicitar info y presentarla
+$('#buscar').click((e) => {
+  if (state.customSearch == false){
+    encerarFiltros();
+  }
+  else if (state.customSearch){
+    setearFiltros();
+  }
+  hacerPeticiones(`data&${JSON.stringify(state.filtros)}`);
+});
--- server/index.js
+++ server/index.js
@@ -11,14 +11,17 @@ const server = http.createServer(async (req, res) => {
     
     //EXPLICACION DE RECIBIR DATOS: cuando usamos createServer le pasamos un callback que recibe req y res. Req es un objeto que viene de la peticion de cliente y res es un objeto de este tipo especifico al cual le damos ciertas caracteristicas para enviarlo con end(); ---- Ahora, el objeto REQ es de tipo http.ClientRequest que implementa stream.Writable y todos los streams son instancias de EventEmitter. Event emitter tiene una funcion para escuchar un evento llamada .on y .once que es igual pero solo eschucha una vez y se cierra. En este caso escuchan que llego el data y el callback recibe el pedazo de data (por eso se llaman streams) y lo anade a los otros pedazos de data. 
 
+    //variables de data para cada busqueda
+    let dataActual = data; 
+    let dataActualJSON = dataJSON;
     //recibir datos del cliente cuando es POST (lo que se manda en request.send)
     var parametrosCliente = '';
     await (()=> {
         req.once('data', function (chunk) {
             parametrosCliente += chunk;
-            console.log(parametrosCliente);
         });
     })();
+    console.log('solicitud recibida: '+ parametrosCliente);
 
     //escribir cabezera
     res.writeHead(200, {
@@ -27,25 +30,58 @@ const server = http.createServer(async (req, res) => {
 		'Access-Control-Allow-Origin': '*',
 		'X-Powered-By':'nodejs'
     });
+    //--------------------------------------------------------------
+    //-------------------ROUTING------------------ filtrar segun los parametros enviados desde el cliente
+    //--------------------------------------------------------------
 
-    if (parametrosCliente === 'ciudades'){
-
-        let ciudades = dataJSON.map((item) => item.Ciudad);
-        ciudades = ciudades.filter((item, index) => ciudades.indexOf(item) >= index);
-        res.write(ciudades.toString());
+    // ---SI SOLICITA LOS VALORES PARA LAS OPCIONES
+    if (parametrosCliente === 'Ciudad' || parametrosCliente === 'Tipo'){
+        let opciones = dataActualJSON.map((item) => item[parametrosCliente]);
+        opciones = opciones.filter((item, index) => opciones.indexOf(item) >= index);
+        res.write(opciones.toString());
+        console.log('respuesta entregada: '+ opciones.toString().substring(0,15) + '...');
     }
-    
-    else if (parametrosCliente === 'tipos'){
-        
-        let tipos = dataJSON.map((item) => item.Tipo);
-        tipos = tipos.filter((item, index) => tipos.indexOf(item) >= index);
-        res.write(tipos.toString());
-    }
-
-    else if (parametrosCliente === "chao"){
-        res.write(data);
+    // --- SI SOLICITA DATOS
+    else if (parametrosCliente.substr(0,4) === "data"){
+        //SI ES DATA Y TIENE MAS PARAMETROS LOS GUARDO EN UN OBJETO
+        parametrosClienteObj = JSON.parse(parametrosCliente.split('&')[1]);
+        //PARA CADA PARAMETRO
+        Object.keys(parametrosClienteObj).forEach(element => {    
+            //SI EL PARAMETRO NO ESTA VACIO
+            if(parametrosClienteObj[element] !== ""){  
+                //SI EL PARAMETRO ES PRECIOMIN O PRECIOMAX
+                if (element == 'precioMin' || element == 'precioMax'){
+                    //QUITO EL EL SIMBOLO $ Y LOS ESPACIOS Y LO HAGO NUMERO (COMO VIENEN DE IONRANGESLIDER VIENEN EN ESE FORMATO)
+                    parametrosClienteObj[element] = parametrosClienteObj[element].substring(1,parametrosClienteObj[element].length).replace(" ","");
+                    if (typeof parametrosClienteObj[element] !== Number) parametrosClienteObj[element] = Number(parametrosClienteObj[element])
+                    //SI ES MIN FILTRO HACIA MAYOR QUE PRECIOMIN
+                    if (element == 'precioMin'){
+                        dataActualJSON = dataActualJSON.filter(item => {
+                            //QUITO EL SIMBOLO $ Y LAS COMAS (QUE ES EL FORMADO DE DATA.JSON)
+                            precio = Number(item["Precio"].substring(1,item["Precio"].length).replace(",",""));
+                            return precio > parametrosClienteObj[element];
+                        });
+                    //SI ES MAX FILTRO HACIA MENOR QUE PRECIOMAX    
+                    } else {
+                        dataActualJSON = dataActualJSON.filter(item => {
+                            precio = Number(item["Precio"].substring(1,item["Precio"].length).replace(",",""));
+                            return precio < parametrosClienteObj[element];
+                        });
+                    }
+                }
+                //SI EL PARAMETRO ES CIUDAD O TIPO
+                else {
+                    //FILTRO LOS ELEMENTOS QUE SEAN IGUALES AL VALOR DEL PARAMETRO
+                    dataActualJSON = dataActualJSON.filter(item => item[element] == parametrosClienteObj[element]);
+                }
+                dataActual= JSON.stringify(dataActualJSON);
+           }
+        });
+        //ESCRIBO LA RESPUESTA AL CLIENTE Y EN LA CONSOLA UN RESUMEN UTIL
+        res.write(dataActual.toString());
+        console.log(`respuesta entregada: ${data.toString().replace(/\s/g,'').substring(0,15)}...] -> length = ${dataActualJSON.length}`);
     } 
-    
+    //SI LOS PARAMETROS SON DE CUALQUIER OTRA FORMA
     else {
         res.write('');
     }
@@ -53,7 +89,6 @@ const server = http.createServer(async (req, res) => {
     //enviar la respuesta
     res.end(); 
     
-    console.log('respuesta entregada');
 });
 
 server.listen(PORT, '127.0.0.1', () => {
